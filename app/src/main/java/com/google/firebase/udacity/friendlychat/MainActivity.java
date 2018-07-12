3dcd488a79b605d5bb7dfdaf3644785de2d178e6
Ankush <ankushransiwal.13@gmail.com>
2018-07-12 13:45:33 +0530

    Add the UI for the sign in using Firebase-UI-Auth

--- app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
@@ -31,7 +31,11 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -39,6 +43,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -47,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String ANONYMOUS = "anonymous";
     public static final int DEFAULT_MSG_LENGTH_LIMIT = 1000;
+    public static final int RC_SIGN_IN = 1;
 
     private ListView mMessageListView;
     private MessageAdapter mMessageAdapter;
@@ -67,6 +73,9 @@ public class MainActivity extends AppCompatActivity {
 
     private ChildEventListener mChildEventListener;
 
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,8 +91,12 @@ public class MainActivity extends AppCompatActivity {
         mMessageEditText = (EditText) findViewById(R.id.messageEditText);
         mSendButton = (Button) findViewById(R.id.sendButton);
 
-        //To get the instance if the class : Provides an entry point
+        //To get the instance of the class : Provides an entry point
         mFirebaseDatabase = FirebaseDatabase.getInstance();
+
+        //Provides an entry in the authentication
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
         //Use the above reference to access a particular portion (Messages) of the database
         mMessagesDatabaseReference = mFirebaseDatabase.getReference().child("messages");
 
@@ -135,7 +148,8 @@ public class MainActivity extends AppCompatActivity {
                 // Clear input box
                 mMessageEditText.setText("");
             }
-        });mChildEventListener = new ChildEventListener() {
+        });
+        mChildEventListener = new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
 
@@ -166,6 +180,30 @@ public class MainActivity extends AppCompatActivity {
         //mChildEventListener is passed to perform methods declared
         mMessagesDatabaseReference.addChildEventListener(mChildEventListener);
 
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user!= null){
+                    //user is signed in
+                    Toast.makeText(MainActivity.this, "The user is signed in. Welcome to FriendlyChat!", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    //user is signed out
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder().setIsSmartLockEnabled(false) //smart lock Automatically saves the user credentials and lock them in
+                                    .setAvailableProviders(Arrays.asList(
+                                            new AuthUI.IdpConfig.GoogleBuilder().build(),
+
+                                            new AuthUI.IdpConfig.EmailBuilder().build()))
+
+                                    .build(),
+                            RC_SIGN_IN);
+                }
+            }
+        };
+
     }
 
     @Override
@@ -178,6 +216,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
     }
 
 }
