77465f12a3857a31bc056ceec44fcfecdfa98a9d
Ankush <ankushransiwal.13@gmail.com>
2018-07-12 18:14:59 +0530

    Add the sign out button feature

--- app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
@@ -188,7 +188,14 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        return super.onOptionsItemSelected(item);
+        switch(item.getItemId()){
+            case R.id.sign_out_menu:
+                AuthUI.getInstance().signOut(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        
     }
 
     @Override
@@ -208,7 +215,7 @@ public class MainActivity extends AppCompatActivity {
             if(resultCOde == RESULT_OK){
                 Toast.makeText(this, "Signed In", Toast.LENGTH_SHORT).show();
             }
-            else if(requestCode == RESULT_CANCELED){
+            else if(resultCOde == RESULT_CANCELED){
                     Toast.makeText(this, "Sign In Cancelled", Toast.LENGTH_SHORT).show();
                     finish();
             }
